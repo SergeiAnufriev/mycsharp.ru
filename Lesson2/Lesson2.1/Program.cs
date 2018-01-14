4d69aa95c4f784fb4ab1a7ddfcf49a88d6d0de2a
SergeiAnufriev <seregioas@gmail.com>
2018-01-14 13:08:51 +0300

    add 2th lesson

---
+++ Lesson2/Lesson2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 5;
+            int b, c;
+            bool d;
+            d = true;
+            long e = 10;
+            float f = 5.5f;
+            char g = 'g';
+
+            int r = 40000;
+            short t = 45;
+            t = (short)r;
+
+            const int monthc = 15;
+
+        }
+    }
+}
