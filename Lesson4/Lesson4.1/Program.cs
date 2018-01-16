8ae19a133d2a9f728d7d09cb1ee608bf18d7a847
SergeiAnufriev <seregioas@gmail.com>
2018-01-16 22:38:46 +0300

    Add 4.1th lesson

---
+++ Lesson4/Lesson4.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Программа для игры с гостями
+            int guest, home;
+            Console.WriteLine("Guest goals:");
+            guest = Convert.ToInt32(Console.ReadLine()); // Гости вводят число
+            Console.WriteLine("Home goals:");
+            home = Convert.ToInt32(Console.ReadLine()); //Хозяева вводят число
+
+            if (guest > home)
+            {
+                Console.WriteLine("The guests won"); // Выиграли гости
+            }
+            else if (guest < home)
+            {
+                Console.WriteLine("Еhe hosts won"); // Выиграли хозяева
+            }
+            else 
+            {
+                Console.WriteLine("Draw"); // Ничья
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
