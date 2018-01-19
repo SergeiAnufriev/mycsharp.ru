0b757d85b64029e2fcd601d30b94dee70a4e47f8
SergeiAnufriev <seregioas@gmail.com>
2018-01-19 18:51:37 +0300

    Add 4.2th lesson

---
+++ Lesson4/Lesson4.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Программа для нахождения чискл кратных 3-м и 7-ми
+            int number;
+            Console.Write("Enter the number:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 3 == 0)  //Нахождение остатка при делении числа на 3
+
+                if (number % 7 == 0)  // Нахождение остатка при делении на 7
+                {
+                    Console.WriteLine("The number is a multiple of 3 and 7");
+                }
+                else
+                {
+                    Console.WriteLine("The number is not a multiple of 3 and 7");
+                }
+            else Console.WriteLine("The number is not a multiple of 3 and 7");
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
