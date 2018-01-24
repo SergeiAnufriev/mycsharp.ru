e9a0a018f252202a919939e7d66d6de4327a592a
SergeiAnufriev <seregioas@gmail.com>
2018-01-24 16:40:43 +0300

    Add 6.1th lesson

---
+++ Lesson6/Lesson6.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Lesson6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Программма для вывода на экран 20-ти последовательных чисел, с шагом 3
+
+            int y = 0;
+
+            for (int i = 1; i < 60; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
