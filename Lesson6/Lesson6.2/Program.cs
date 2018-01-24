97689e46421d40fe0fad0ad8bfcc1c58d78f1daa
SergeiAnufriev <seregioas@gmail.com>
2018-01-24 16:41:00 +0300

    Add 6.2th lesson

---
+++ Lesson6/Lesson6.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Программа для проверки введённого пороля
+            string password;
+            do
+            {
+                Console.Write("Enter password:");
+                password = Convert.ToString(Console.ReadLine());
+            }
+            while (password != "root");
+
+
+
+        }
+    }
+}
