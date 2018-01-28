8f8a64ad4e351b5fea99b879bdfff707bd0df325
SergeiAnufriev <seregioas@gmail.com>
2018-01-28 17:06:29 +0300

    Add 7th lesson

---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson7
+{
+    class Program
+    {
+        // Программа выводит на экран, с помощью цикла "Foreach" все элементы массива целых чисел, которые больше 20 и меньше 50.
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 100)
+            {
+                int[] numbers = { i++ };
+
+                foreach (int el in numbers)
+                {
+
+                    if (i < 21)
+                        continue;
+
+                    if (i > 50)
+                        break;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
