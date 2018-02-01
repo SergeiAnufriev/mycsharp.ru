f66cbcf6af635b6f65d778074aa8d790615786ec
SergeiAnufriev <seregioas@gmail.com>
2018-02-01 20:12:04 +0300

    Add 9.3th lesson

---
+++ Lesson9/Lesson9.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson9._3
+{
+    class Program
+    {
+        // Программа разбивает строку на массив строк, и переводит их в нижний регистр
+
+        static void Main(string[] args)
+        {
+            string s = "Login1,LOgin2,login3,loGin4";
+            string[] array = s.Split(",");
+            string x = "";
+
+            foreach (string el in array)
+            {
+                x += el;
+            }
+
+            x = x.ToLower();
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
