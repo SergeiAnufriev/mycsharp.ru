29fe128002db9b4e8be4bef12d97df0d42d8922a
SergeiAnufriev <seregioas@gmail.com>
2018-02-01 18:03:38 +0300

    Add 9.1th lesson

---
+++ Lesson9/Lesson9.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson9._1
+{
+    class Program
+    {
+        // Программа заменяет в строке имя "Nikolay" на "Oleg"
+
+        static void Main(string[] args)
+        {
+            string s = "Nikolay, Ivan, Pert, Vladimir, Nikolay, Aleksandr";
+            string change = s.Replace("Nikolay", "Oleg");
+
+            Console.WriteLine(change);
+            Console.ReadKey();
+        }
+    }
+}
