439c57658e4f112ef614e5becfc8049b3db47e73
SergeiAnufriev <seregioas@gmail.com>
2018-02-01 18:20:10 +0300

    Add 9.2th lesson

---
+++ Lesson9/Lesson9.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson9._2
+{
+    class Program
+    {
+        // Программа выводит на экран часть текста, из строки
+
+        static void Main(string[] args)
+        {
+            string s = "Сегодня мы с вами рассмотрели, как работать со строками в Си-шарп. Были описаны основные операторы и методы, которые используются для работы со строками";
+            string excerpt = s.Substring(67, 40);
+
+            Console.WriteLine(excerpt);
+            Console.ReadKey();
+        }
+    }
+}
